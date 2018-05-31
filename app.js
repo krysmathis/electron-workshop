28ef40643633e1223a5db50884956ff88db2f402
krysmathis <krysmathis@gmail.com>
2018-05-31 09:52:11 -0500

    ipc messaging

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+const { ipcRenderer } = require( "electron" );
+
 document.addEventListener( "DOMContentLoaded", () => {
+    
     const version = process.version;
     const e = document.getElementById("info");
     e.textContent = `I'm running Node.js version: ${version}`;
-})
+    
+    const btn = document.querySelector("#clickme");
+    btn.addEventListener( "click", e => {
+        console.log( "I was clicked." );
+        ipcRenderer.send( "show-dialog", { message: "The button was cliked."});
+    });
+});
+
+ipcRenderer.on("response-dialog", (e, arg) => {
+    if (arg === "ping") {
+        console.log("pong");
+    }
+    
+    ipcRenderer.sendSync("sync-response-to-dialog","score!");
+});
--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 "use strict";
 
 const electron = require("electron");
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain, dialog } = electron;
 const path = require("path");
 const reload = require("electron-reload");
 const isDev = require("electron-is-dev");
+
 let mainWindow = null;
 
 if (isDev) {
-    const electronPath = path.join(__dirname, "node_modules", "bin", "electron");
+    const electronPath = path.join(__dirname, "node_modules", ".bin", "electron");
     reload(__dirname, {electron: electronPath });
 }
 
@@ -21,10 +22,33 @@ app.on( "window-all-closed", () => {
 app.on( "ready", () => {
     mainWindow = new BrowserWindow ( { width: 800, height: 600 })
     mainWindow.loadURL( `file://${__dirname}/index.html`);
+    if ( isDev ) {
+        mainWindow.webContents.openDevTools();
+    }
     mainWindow.once(" ready-to-show", () => {
         mainWindow.show();
     });
     mainWindow.on( "closed", () => {
         mainWindow = null;
     });
-})
+});
+
+ipcMain.on( "show-dialog", (e, arg) => {
+    const msgInfo = {
+        title: "My App Alert", 
+        message: arg.message,
+        buttons: ["OK"]
+    };
+    dialog.showMessageBox( msgInfo );
+    e.sender.send("response-dialog", "ping");
+} );
+
+ipcMain.on("sync-response-to-dialog", (e, arg) => {
+    console.log("copy that.");
+    const msgInfo = {
+        title: "My App Alert", 
+        message: "copy that",
+        buttons: ["OK"]
+    };
+    dialog.showMessageBox( msgInfo );
+});
